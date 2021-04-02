dd9ceeea72ffc840596d9da9d702d6f72aed9b7f
Mike Flynn <mdflynn34@outlook.com>
2021-04-02 09:46:09 -0600

    add definitions

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const amount = 12;
+// GLOBALS  - NO WINDOW !!!!
 
-if (amount < 10 ) {
-    console.log('small number');
-} else {
-    console.log('large number')
-}
-
-console.log('hello world');
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function to use modules (CommonJS)
+// module     - info about current module (file)
+// process    - info about env where the program is being executed
+setInterval(() => {
+    console.log('hello world again');
+}, 1000);
