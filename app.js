4b8ac9d78670ce1e5b5542f5f25d5e96ef333099
Mike Flynn <mdflynn34@outlook.com>
2021-04-02 10:15:27 -0600

    show alternative export types

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 // Modules - Encapsulated Code (only share minimum)
 const { john, peter } = require("./4-names");
 const sayHi = require("./5-utils");
+const data = require("./6-alternative-flavor");
 
 sayHi("Mike");
 sayHi(john);
