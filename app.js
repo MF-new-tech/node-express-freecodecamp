3d95df38c25b7d35107e5149ddfc3e5ff841caa3
Mike Flynn <mdflynn34@outlook.com>
2021-04-05 12:46:27 -0600

    initial commit

--- app.js
+++
@@ -1,9 +0,0 @@
-// CommonJS, every file is module (by default)
-// Modules - Encapsulated Code (only share minimum)
-const { john, peter } = require("./4-names");
-const sayHi = require("./5-utils");
-const data = require("./6-alternative-flavor");
-
-sayHi("Mike");
-sayHi(john);
-sayHi(peter);
